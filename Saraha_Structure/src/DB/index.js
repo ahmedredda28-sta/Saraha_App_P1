278c7fe4d9aaa10bd8346f4bc74ea5e6a2d6f162
Your Name <you@example.com>
2026-02-22 14:07:19 +0200

    Updated homepage layout and added new images folder

--- Saraha_Structure/src/DB/index.js
+++
@@ -1,6 +0,0 @@
-import { createConnection } from './connection.db';
-import * as repository from './database.repository';
-
-const dbConnection = createConnection();
-
-export { dbConnection, repository };
